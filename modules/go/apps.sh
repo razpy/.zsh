f7781735638b81cfc1fbdb37390c6ca5ed9240a8
razpy <28157743+razpy@users.noreply.github.com>
2021-04-04 22:29:40 +0900

    typeの結果のエラーを出力しない

--- modules/go/apps.sh
+++ modules/go/apps.sh
@@ -2,7 +2,7 @@
 if [ -d $HOME/.gvm/scripts/gvm ];then
   source $HOME/.gvm/scripts/gvm
 
-  if type go > /dev/null; then
+  if type go > /dev/null 2>&1 ; then
     # powerline-goを呼び出す関数をprecmdに登録
     if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
       install_powerline_precmd
